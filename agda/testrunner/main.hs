783f3f310c01604b61a88af6ac875c5dc2a4ab97
Jesper Cockx <jesper@sikanda.be>
2022-03-28 11:37:22 +0200

    Add possibility to give weights to test cases

--- agda/testrunner/main.hs
+++ agda/testrunner/main.hs
@@ -6,14 +6,15 @@ import Data.Char
 import System.Exit
 import System.IO
 import System.Process
+import Text.Read ( readMaybe )
 
 import qualified Text.XML.Light as XML
 
--- Test name + full code
-type TestCase = (String, [String])
+-- Test name + weight + full code
+type TestCase = (String, Int, [String])
 
--- Test name + either error or nothing
-type TestResult = (String, Either String ())
+-- Test name + weight + either error or nothing
+type TestResult = (String, Int, Either String ())
 
 outputFile = "output/results.xml"
 
@@ -29,14 +30,14 @@ main = do
   (exit, out, err) <- readProcessWithExitCode "agda" ["Solution.agda"] ""
   case exit of
     ExitSuccess{} -> writeResults outputFile =<< runTests
-    ExitFailure{} -> writeResults outputFile [("typechecking",Left out)]
+    ExitFailure{} -> writeResults outputFile [("typechecking failed",1,Left out)]
 
 runTests :: IO [TestResult]
 runTests = do
   tst <- readFileUTF8 "test.agda"
   let tests = getTests tst
 
-  forM tests $ \(testName,testBody) -> do
+  forM tests $ \(testName,testWeight,testBody) -> do
 
     let testFile = testName++".agda"
 
@@ -49,11 +50,12 @@ runTests = do
     (exit, out, err) <- readProcessWithExitCode "agda" [testFile] ""
 
     case exit of
-      ExitSuccess{} -> return (testName, Right ())
-      ExitFailure{} -> return (testName, Left out)
+      ExitSuccess{} -> return (testName, testWeight, Right ())
+      ExitFailure{} -> return (testName, testWeight, Left out)
 
--- Get the names and code of all tests in the file with the
--- given content (names starting with `test-`).
+-- Get the names and code of all tests in the file with the given
+-- content (names starting with `test-` or `test-x-` for a test case
+-- with weight x).
 getTests :: String -> [TestCase]
 getTests fileContents = loop id $ lines fileContents
   where
@@ -63,19 +65,32 @@ getTests fileContents = loop id $ lines fileContents
       let name = defName curr in
       if (take 5 name == "test-") then
         let (test,rest') = span (\l -> null l || isSpace (head l) || defName l == name) rest
-        in  (name , prev (curr:test)) : loop prev rest'
+        in  (name , getWeight name , prev (curr:test)) : loop prev rest'
       else loop (prev . (curr:)) rest
 
     defName = takeWhile $ not . isSpace
 
+    getWeight name = case readMaybe (takeWhile (/= '-') (drop 5 name)) of
+      Just (n :: Int) -> n
+      Nothing         -> 1
+
+nameAttr :: String -> XML.Attr
+nameAttr n = XML.Attr (XML.unqual "name") n
+
+weightAttr :: Int -> XML.Attr
+weightAttr w = XML.Attr (XML.unqual "weight") (show w)
 
 formatResult :: TestResult -> XML.Element
-formatResult (testName,r) = case r of
-  Right _  -> XML.unode "testcase" ()
-  Left err -> XML.unode "testcase" $ XML.unode "failure" $
-                    unlines [ "Test " ++ testName ++ " failed!"
-                            , err
-                            ]
+formatResult (testName,testWeight,r) = case r of
+  Right _  -> XML.unode "testcase" attributes
+  Left err -> XML.unode "testcase" (attributes, failureNode err)
+
+  where
+    attributes = [nameAttr testName , weightAttr testWeight]
+    failureNode err = XML.unode "failure" $
+      unlines [ "Test " ++ testName ++ " failed!"
+              , err
+              ]
 
 formatResults :: [TestResult] -> XML.Element
 formatResults rs =
--- agda/tests/test_halve/test.agda
+++ agda/tests/test_halve/test.agda
@@ -1,14 +1,14 @@
 
 open import Agda.Builtin.Equality
 
-test-halve0 : halve 0 ≡ 0
-test-halve0 = refl
+test-2-halve0 : halve 0 ≡ 0
+test-2-halve0 = refl
 
-test-halve1 : halve 1 ≡ 0
-test-halve1 = refl
+test-3-halve1 : halve 1 ≡ 0
+test-3-halve1 = refl
 
-test-halve8 : halve 8 ≡ 4
-test-halve8 = refl
+test-4-halve8 : halve 8 ≡ 4
+test-4-halve8 = refl
 
-test-halve13 : halve 13 ≡ 6
-test-halve13 = refl
+test-6-halve13 : halve 13 ≡ 6
+test-6-halve13 = refl
